bf92edbf4045480050c368df7cac4064cc78fe7f
Matt Bush <59295261+matthewbush55@users.noreply.github.com>
2021-08-28 14:55:35 -0600

    Updated notes.js to include delete functionality

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,7 @@
 const notes = require("express").Router();
 const util = require("util");
 const fs = require("fs");
+const app = require(".");
 
 const readFromFile = util.promisify(fs.readFile);
 // used to create a new id for each individual note
@@ -8,7 +9,6 @@ const uuid = () =>
   Math.floor((1 + Math.random()) * 0x10000)
     .toString(16)
     .substring(1);
-
 const readAndAppend = (content, filePath) => {
   fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
@@ -21,6 +21,18 @@ const readAndAppend = (content, filePath) => {
   });
 };
 
+const readAndDelete = (content, filePath) => {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      const deleteIndex = parsedData.findIndex((i) => i.id === content);
+      parsedData.splice(deleteIndex, 1);
+      writeToFile(filePath, parsedData);
+    }
+  });
+};
 const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
@@ -56,6 +68,15 @@ notes.post("", (req, res) => {
   } else {
     res.json("Error in posting feedback");
   }
+});
+
+// DELETE request
+notes.delete("", (req, res) => {
+  console.log(req.params[0]);
+  console.log(`${req.method} request received for /api/notes`);
+  const deletedNote = req.params[0];
+  readAndDelete(deletedNote, "./db/db.json");
+  res.json("Delete success");
 });
 
 module.exports = notes;
